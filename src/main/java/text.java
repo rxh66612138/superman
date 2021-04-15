e5843b71698f3ca046875ad9fa20729cdf22eefc
rxh2 <rxh11160524@163.com>
2021-04-15 19:44:30 +0800

    first

--- src/main/java/text.java
+++ src/main/java/text.java
@@ -1,5 +1,9 @@
 public class text {
     public static void main(String[] args) {
         System.out.println("first");
+
+        System.out.println("second");
+
+        System.out.println("thired");
     }
 }
